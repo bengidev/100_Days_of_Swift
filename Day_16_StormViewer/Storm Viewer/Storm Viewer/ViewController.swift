d1ce38e213dd97be713c42093f188aa4a1c58257
syndicate017 <bambang.trd17@gmail.com>
2023-10-13 23:57:43 +0700

    Day 16 StormViewer from 100 Days of Swift

---
+++ Day_16_StormViewer/Storm Viewer/Storm Viewer/ViewController.swift
@@ -0,0 +1,46 @@
+//
+//  ViewController.swift
+//  Storm Viewer
+//
+//  Created by Bambang Tri Rahmat Doni on 13/10/23.
+//
+
+import UIKit
+
+/// Designing our interface
+///
+class ViewController: UITableViewController {
+    private var pictures = [String]()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        /// Listing images with FileManager
+        ///
+        let fm = FileManager.default
+        let path = Bundle.main.resourcePath!
+        let items = try! fm.contentsOfDirectory(atPath: path)
+        
+        for item in items {
+            if item.hasPrefix("nssl") {
+                pictures.append(item)
+            }
+        }
+        
+        print(pictures)
+    }
+
+    /// Designing our interface
+    ///
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return pictures.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Picture", for: indexPath)
+        cell.textLabel?.text = pictures[indexPath.row]
+        return cell
+    }
+
+}
+
