d9008eb904c14a5f9731776b99b991a3a60329ab
syndicate017 <bambang.trd17@gmail.com>
2023-10-15 00:40:55 +0700

    Day 24 EasyBrowser from 100 Days of Swift

---
+++ Day_24_EasyBrowser/Easy Browser/Easy Browser/ViewController.swift
@@ -0,0 +1,56 @@
+//
+//  ViewController.swift
+//  Easy Browser
+//
+//  Created by Bambang Tri Rahmat Doni on 14/10/23.
+//
+
+import UIKit
+import WebKit
+
+/// Creating a simple browser with WKWebView
+///
+class ViewController: UIViewController, WKNavigationDelegate {
+    private var webView: WKWebView!
+    
+    override func loadView() {
+        webView = WKWebView()
+        webView.navigationDelegate = self
+        view = webView
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Open",
+                                                            style: .plain,
+                                                            target: self,
+                                                            action: #selector(openTapped))
+        
+        let url = URL(string: "https://www.hackingwithswift.com")!
+        webView.load(URLRequest(url: url))
+        webView.allowsBackForwardNavigationGestures = true
+    }
+
+    /// Choosing a website: UIAlertController action sheets
+    ///
+    @objc private func openTapped() -> Void {
+        let ac = UIAlertController(title: "Open Page", message: nil, preferredStyle: .actionSheet)
+        ac.addAction(UIAlertAction(title: "apple.com", style: .default, handler: openPage))
+        ac.addAction(UIAlertAction(title: "hackingwithswift.com", style: .default, handler: openPage))
+        ac.addAction(UIAlertAction(title: "Cancel", style: .cancel))
+        ac.popoverPresentationController?.barButtonItem = self.navigationItem.rightBarButtonItem
+        present(ac, animated: true)
+    }
+    
+    private func openPage(action: UIAlertAction) -> Void {
+        guard let title = action.title else { return }
+        guard let url = URL(string: "https://" + title) else { return }
+        webView.load(URLRequest(url: url))
+    }
+    
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        title = webView.title
+    }
+}
+
