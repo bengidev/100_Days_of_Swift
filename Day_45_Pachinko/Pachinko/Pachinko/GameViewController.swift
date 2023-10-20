ec2c37366e25d721a548fcd31c8fb5db51525419
syndicate017 <bambang.trd17@gmail.com>
2023-10-21 00:11:50 +0700

    Day 45 Pachinko from 100 Days of Swift

---
+++ Day_45_Pachinko/Pachinko/Pachinko/GameViewController.swift
@@ -0,0 +1,40 @@
+//
+//  GameViewController.swift
+//  Pachinko
+//
+//  Created by Bambang Tri Rahmat Doni on 20/10/23.
+//
+
+import UIKit
+import SpriteKit
+
+class GameViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        if let view = self.view as! SKView? {
+            // Load the SKScene from 'GameScene.sks'
+            if let scene = SKScene(fileNamed: "GameScene") {
+                // Set the scale mode to scale to fit the window
+                scene.scaleMode = .aspectFill
+                
+                // Present the scene
+                view.presentScene(scene)
+            }
+            
+            view.ignoresSiblingOrder = true
+            
+            view.showsFPS = true
+            view.showsNodeCount = true
+        }
+    }
+
+    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
+        return .all
+    }
+
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
+}
