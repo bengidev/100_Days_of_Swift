964e3cddebce2273cd21d7ef06508a285a5123d8
syndicate017 <bambang.trd17@gmail.com>
2023-10-20 22:54:54 +0700

    Day 43 NamesToFaces from 100 Days of Swift

---
+++ Day_43_NamesToFaces/Names To Faces/Names To Faces/ViewController.swift
@@ -0,0 +1,96 @@
+//
+//  ViewController.swift
+//  Names To Faces
+//
+//  Created by Bambang Tri Rahmat Doni on 20/10/23.
+//
+
+import UIKit
+
+class ViewController: UICollectionViewController,
+                        UIImagePickerControllerDelegate,
+                        UINavigationControllerDelegate {
+    private var people = [Person]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add,
+                                                           target: self,
+                                                           action: #selector(addNewPerson))
+    }
+
+    override func collectionView(_ collectionView: UICollectionView,
+                                 numberOfItemsInSection section: Int) -> Int {
+        return people.count
+    }
+
+    override func collectionView(_ collectionView: UICollectionView,
+                                 cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Person",
+                                                            for: indexPath) as? PersonCell
+        // We failed to get a PersonCell – bail out!
+        else { fatalError("Unable to dequeue PersonCell.") }
+        
+        let person = people[indexPath.item]
+        let imagePath = getDocumentsDirectory().appending(path: person.image, directoryHint: .isDirectory)
+        
+        cell.name.text = person.name
+        cell.imageView.image = UIImage(contentsOfFile: imagePath.path())
+        cell.imageView.layer.borderColor = UIColor(white: 0, alpha: 0.3).cgColor
+        cell.imageView.layer.borderWidth = 2
+        cell.imageView.layer.cornerRadius = 3
+        cell.layer.cornerRadius = 7
+        
+        // If we're still here it means we got a PersonCell, so we can return it
+        return cell
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        let person = people[indexPath.item]
+        
+        let ac = UIAlertController(title: "Rename Person", message: nil, preferredStyle: .alert)
+        
+        ac.addTextField()
+        ac.addAction(UIAlertAction(title: "OK", style: .default) {
+            [weak self, weak ac] _ in
+            guard let newName = ac?.textFields?.first?.text else { return }
+            person.name = newName
+            
+            self?.collectionView.reloadData()
+        })
+        ac.addAction(UIAlertAction(title: "Cancel", style: .cancel))
+        
+        present(ac, animated: true)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController,
+                               didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        guard let image = info[.editedImage] as? UIImage else { return }
+        
+        let imageName = UUID().uuidString
+        let imagePath = getDocumentsDirectory().appending(path: imageName, directoryHint: .isDirectory)
+        
+        if let jpegData = image.jpegData(compressionQuality: 0.8) {
+            try? jpegData.write(to: imagePath)
+        }
+        
+        let person = Person(name: "Unknown", image: imageName)
+        people.append(person)
+        collectionView.reloadData()
+        
+        dismiss(animated: true)
+    }
+    
+    @objc private func addNewPerson() -> Void {
+        let picker = UIImagePickerController()
+        picker.allowsEditing = true
+        picker.delegate = self
+        present(picker, animated: true)
+    }
+    
+    private func getDocumentsDirectory() -> URL {
+        let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        return paths[0]
+    }
+}
