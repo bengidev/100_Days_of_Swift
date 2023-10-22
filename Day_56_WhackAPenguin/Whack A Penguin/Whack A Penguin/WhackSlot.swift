7c66391800bb2b283f88afb6ddda54199f65e558
syndicate017 <bambang.trd17@gmail.com>
2023-10-22 20:37:55 +0700

    Day 55 WhackAPenguin from 100 Days of Swift

---
+++ Day_56_WhackAPenguin/Whack A Penguin/Whack A Penguin/WhackSlot.swift
@@ -0,0 +1,62 @@
+//
+//  WhackSlot.swift
+//  Whack A Penguin
+//
+//  Created by Bambang Tri Rahmat Doni on 22/10/23.
+//
+
+import SpriteKit
+import UIKit
+
+class WhackSlot: SKNode {
+    private var charNode: SKSpriteNode!
+    
+    private var isVisible = false
+    private var isHit = false
+
+    func configure(at position: CGPoint) -> Void {
+        self.position = position
+        
+        let sprite = SKSpriteNode(imageNamed: "whackHole")
+        addChild(sprite)
+        
+        let cropNode = SKCropNode()
+        cropNode.position = CGPoint(x: 0, y: 15)
+        cropNode.zPosition = 1
+        cropNode.maskNode = SKSpriteNode(imageNamed: "whackMask")
+        
+        charNode = SKSpriteNode(imageNamed: "penguinGood")
+        charNode.position = CGPoint(x: 0, y: -90)
+        charNode.name = "character"
+        cropNode.addChild(charNode)
+        
+        addChild(cropNode)
+    }
+    
+    func show(hideTime: Double) -> Void {
+        if isVisible { return }
+        
+        charNode.run(SKAction.moveBy(x: 0, y: 80, duration: 0.05))
+        isVisible = true
+        isHit = false
+        
+        if Int.random(in: 0...2) == 0 {
+            charNode.texture = SKTexture(imageNamed: "penguinGood")
+            charNode.name = "charFriend"
+        } else {
+            charNode.texture = SKTexture(imageNamed: "penguinEvil")
+            charNode.name = "charEnemy"
+        }
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + (hideTime * 3.5)) { [weak self] in
+            self?.hide()
+        }
+    }
+    
+    func hide() -> Void {
+        if !isVisible { return }
+        
+        charNode.run(SKAction.moveBy(x: 0, y: -80, duration: 0.05))
+        isVisible = false
+    }
+}
