80a72aa1f67365e87d5a4ecb35ad1e46915a8389
syndicate017 <bambang.trd17@gmail.com>
2023-10-24 21:43:29 +0700

    Day 68 JavaScriptInjection from 100 Days of Swift

---
+++ Day_68_JavaScriptInjection/JavaScript Injection/Extension/ActionViewController.swift
@@ -0,0 +1,86 @@
+//
+//  ActionViewController.swift
+//  Extension
+//
+//  Created by Bambang Tri Rahmat Doni on 24/10/23.
+//
+
+import UIKit
+import MobileCoreServices
+import UniformTypeIdentifiers
+
+class ActionViewController: UIViewController {
+    @IBOutlet var script: UITextView!
+    
+    private var pageTitle = ""
+    private var pageURL = ""
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .done,
+                                                            target: self,
+                                                            action: #selector(done))
+        
+        let notificationCenter = NotificationCenter.default
+        notificationCenter.addObserver(self,
+                                       selector: #selector(adjustForKeyboard),
+                                       name: UIResponder.keyboardWillHideNotification,
+                                       object: nil)
+        notificationCenter.addObserver(self,
+                                       selector: #selector(adjustForKeyboard),
+                                       name: UIResponder.keyboardWillChangeFrameNotification,
+                                       object: nil)
+        
+        if let inputItem = extensionContext?.inputItems.first as? NSExtensionItem {
+            if let itemProvider = inputItem.attachments?.first {
+                itemProvider.loadItem(forTypeIdentifier: UTType.propertyList.identifier as String) {
+                    [weak self] (dict, error) in
+                    guard let itemDictionary = dict as? NSDictionary else { return }
+                    guard let javaScriptValues = itemDictionary[NSExtensionJavaScriptPreprocessingResultsKey]
+                            as? NSDictionary else { return }
+                    
+                    self?.pageTitle = javaScriptValues["title"] as? String ?? ""
+                    self?.pageURL = javaScriptValues["URL"] as? String ?? ""
+                    
+                    DispatchQueue.main.async {
+                        self?.title = self?.pageTitle
+                    }
+                }
+            }
+        }
+    }
+
+    @objc private func done() -> Void {
+        let item = NSExtensionItem()
+        let argument: NSDictionary = ["customJavaScript": script.text ?? ""]
+        let webDictionary: NSDictionary = [NSExtensionJavaScriptFinalizeArgumentKey: argument]
+        let customJavaScript = NSItemProvider(item: webDictionary,
+                                              typeIdentifier: UTType.propertyList.identifier as String)
+        item.attachments = [customJavaScript]
+        
+        extensionContext?.completeRequest(returningItems: [item])
+    }
+
+    @objc private func adjustForKeyboard(notification: Notification) -> Void {
+        guard let keyboardValue = notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue else { return }
+        
+        let keyboardScreenEndFrame = keyboardValue.cgRectValue
+        let keyboardViewEndFrame = view.convert(keyboardScreenEndFrame, from: view.window)
+        
+        if notification.name == UIResponder.keyboardWillHideNotification {
+            script.contentInset = .zero
+        } else {
+            script.contentInset = UIEdgeInsets(top: 0,
+                                               left: 0,
+                                               bottom: keyboardViewEndFrame.height - view.safeAreaInsets.bottom,
+                                               right: 0)
+        }
+        
+        script.scrollIndicatorInsets = script.contentInset
+        
+        let selectedRange = script.selectedRange
+        script.scrollRangeToVisible(selectedRange)
+        
+    }
+}
